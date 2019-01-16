df3d2666450278120134785b1ccd2bc4e8b155a1
Adam Lodowski <adamlodowski@gmail.com>
2019-01-16 15:49:02 +0000

    Added functionality to edit and delete messages

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,53 @@
 var angryButton = document.querySelector(".Send_Message")
 var angryMessage = document.querySelector(".Angry_Message")
+var deleteButton = document.querySelector(".Delete")
+var editButton = document.querySelector(".Edit")
 var messageArray = []
+var messageId = 1
 
-function send_message() {
+function save_message() {
   let userInput = document.querySelector('.user_input').value; 
-  let message = new Message(userInput, new Date)
+  let message = new Message(userInput, new Date, messageId);
+  messageArray.unshift(message);
+  messageId +=1
+}
 
-  messageArray.unshift(message)
-  
+function print_messages() {
   var printThis = "Your messages: <br>"
   messageArray.forEach(function(message) {
-    printThis += "<br> Message: " + message.content + " <br> Sent at: " + message.createdAt + "<br>";
+    printThis += "<br> Message: " + message.content + " <br> Sent at: " + message.createdAt + "<br>" + "Message Number: " + message.id + "<br>";
   })
   angryMessage.innerHTML = printThis
 }
 
+function edit_message() {
+  var messageNum = prompt("To select a message, enter its message number:");
+  var newContent = prompt("What should the message now say?");
+  toEdit = messageArray[messageNum-1]
+  toEdit.content = newContent
+  console.log(toEdit)
+  print_messages();
+}
+
+editButton.onclick = function() {
+  edit_message()
+}
+
 angryButton.onclick = function() {
-  send_message();
+  save_message();
+  print_messages();
+}
+
+deleteButton.onclick = function() {
+  messageArray = [];
+  print_messages();
 }
 
 class Message {
-  constructor(content, createdAt){
+  constructor(content, createdAt, id){
     this.content = content;
     this.createdAt = createdAt;
+    this.id = id
   }
 
   // timeStamp(createdAt) {
